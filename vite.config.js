3a860385b72da5a9383c0ba7741ced81899cd8ab
HYH! <huangyinhaow@outlook.com>
2023-07-01 12:44:06 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			strategies: 'injectManifest',
-			srcDir: 'src',
-			filename: 'sw.js'
-		})
-	]
+	plugins: [sveltekit()]
 });
